96cf8b176a1dce5687cfdb6bfa1dba0fa2c10a91
Tommy Tran <tommy@tmytrn.com>
2022-10-17 10:35:27 -0500

    base home page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f562416d76f15e9e"; }
+/******/ 		__webpack_require__.h = function() { return "61a6346bf74a078f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
