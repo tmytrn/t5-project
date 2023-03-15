3539479e40066b9d668ce42f53c46675132b4090
Tommy Tran <tommy@tmytrn.com>
2023-03-15 15:23:48 -0500

    fixing desk redirect

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 const STUDIO_REWRITE = {
   source: "/admin/:path*",
-  destination:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3333/admin/:path*"
-      : "/admin/index.html",
+  destination: "/admin/desk",
 };
 
 module.exports = {
   reactStrictMode: false,
   swcMinify: true,
+  rewrites: async () => [STUDIO_REWRITE],
   images: {
     remotePatterns: [
       {
