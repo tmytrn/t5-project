a1360c3d0afb8745c7ae3d0d85a0ab3ae1c52be6
Tommy Tran <tommy@tmytrn.com>
2023-09-29 16:28:55 -0500

    another attempt to fix the admin side

--- sanity.cli.js
+++ sanity.cli.js
@@ -1,12 +1,10 @@
-// sanity.cli.js
+import { loadEnvConfig } from "@next/env";
 import { defineCliConfig } from "sanity/cli";
 
-export default defineCliConfig({
-  api: {
-    projectId: "258g4cwf",
-    dataset: "production",
-  },
-  project: {
-    basePath: "/admin",
-  },
-});
+const dev = process.env.NODE_ENV !== "production";
+loadEnvConfig(__dirname, dev, { info: () => null, error: console.error });
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = "production";
+
+export default defineCliConfig({ api: { projectId, dataset } });
